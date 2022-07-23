9086edb9b99b7f9076030dbc19ad45d997447257
Steph <xsabienx@gmail.com>
2022-07-23 12:36:16 -0400

    Node Express  Week One Workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,29 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
+    res.end(`Will add the campsite: ${req.params.campsiteId} with description: ${req.body.description}`);
+})
+
+.post((req,res) => {
+    res.statusCode = 403;
+    res.end(`Post operation not supported on /campsites/${req.params.campsiteId}`);
+})
+
+.put((req, res) => {
+    res.end(`Will update the campsites: ${req.body.name} with description ${req.body.description}`)
+})
+
+.delete((req, res) => {
+    res.end(`Deleting all campsites of id${req.params.campsiteId}`);
+})
+
+
 module.exports = campsiteRouter;
